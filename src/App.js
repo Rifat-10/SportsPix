811cc11748a7d96faa9a3941b7f85ae6f1c00f02
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-04-23 02:19:44 +0600

    404 page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
 import { Route, Routes } from 'react-router-dom';
-
+import NotFound from './Components/NotFound/NotFound';
 function App() {
   return (
     <div className='App bg-dark'>
       <NavBar></NavBar>
-      
+      <div>
+        <Routes>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
