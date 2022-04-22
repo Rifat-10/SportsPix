066b82f85341334434190eddd51c20e738a6b41b
Rifat-10 <mehedihassanrifat.3@gmail.com>
2022-04-23 04:50:41 +0600

    footer  added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './Components/NotFound/NotFound';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className='App bg-dark'>
@@ -13,6 +14,7 @@ function App() {
           <Route path='*' element={<NotFound />}></Route>
         </Routes>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
